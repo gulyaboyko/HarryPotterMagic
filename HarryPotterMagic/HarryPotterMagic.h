134ebfdc4f48914e494378881d22a0a84a581a5f
Gulya Akhtyamova <gulya.nadym@gmail.com>
2020-05-03 20:55:45 +0300

    Initial Commit

---
+++ HarryPotterMagic/HarryPotterMagic.h
@@ -0,0 +1,19 @@
+//
+//  HarryPotterMagic.h
+//  HarryPotterMagic
+//
+//  Created by Gulya Boiko on 5/3/20.
+//  Copyright © 2020 com.gulya.boiko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HarryPotterMagic.
+FOUNDATION_EXPORT double HarryPotterMagicVersionNumber;
+
+//! Project version string for HarryPotterMagic.
+FOUNDATION_EXPORT const unsigned char HarryPotterMagicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HarryPotterMagic/PublicHeader.h>
+
+
